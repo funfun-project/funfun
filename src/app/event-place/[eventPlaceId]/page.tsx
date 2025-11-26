f4b35f737b93bd1ec3e5ad07558d93a6ff194eec
Jedonge <jedonge1995@gmail.com>
2025-11-27 02:02:10 +0900

    fix: eventPlaceId page props 타입 수정

--- src/app/event-place/[eventPlaceId]/page.tsx
+++ src/app/event-place/[eventPlaceId]/page.tsx
@@ -1,3 +1,9 @@
-export default function EventPlaceDetailPage({ params }: { params: { eventPlaceId: string } }) {
-  return <div>Event Place Detail Page for {params['eventPlaceId']}</div>;
+interface PageProps {
+  params: {
+    eventPlaceId: string;
+  };
+}
+
+export default function EventPlaceDetailPage({ params }: PageProps) {
+  return <div>Event Place Detail Page for {params.eventPlaceId}</div>;
 }
