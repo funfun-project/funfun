084bfc5cb2b0f824f386a602cd07a76c0c4e11e9
JUNHORANG <sjsk3358@naver.com>
2026-01-05 19:05:31 +0900

    feat : 하트 버튼 기능, write fab ui

--- src/views/map/components/ListCard.tsx
+++ src/views/map/components/ListCard.tsx
@@ -1,5 +1,6 @@
 import HeartButton from '@/common/HeartButton';
 export default function ListCard() {
+  const initialIsLiked = 'like를 했는지 안 했는지의 값';
   return (
     <>
       <li>
@@ -12,7 +13,11 @@ export default function ListCard() {
               <p className="text-[12px]">2025.07.29 - 2025.08.03</p>
             </div>
             <div className="flex items-center">
-              <HeartButton />
+              <HeartButton
+                initialIsLiked={initialIsLiked ? true : false}
+                goToUrl={''}
+                itemId={''}
+              />
             </div>
           </div>
         </div>
