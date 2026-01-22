a5ec1efa82950e87bc03cb754f10dcff415fb5ec
dlakrp123 <dlakrp731@gmail.com>
2026-01-22 20:50:44 +0900

    feat: Add dynamic detail pages for event places and user announcements.

--- src/app/event-place/[event-place-Id]/page.tsx
+++ src/app/event-place/[event-place-Id]/page.tsx
@@ -1,3 +1,8 @@
-export default function EventPlaceDetailPage({ params }: { params: { 'event-place-Id': string } }) {
-  return <div>Event Place Detail Page for {params['event-place-Id']}</div>;
+export default async function EventPlaceDetailPage({
+  params,
+}: {
+  params: Promise<{ 'event-place-Id': string }>;
+}) {
+  const { 'event-place-Id': eventPlaceId } = await params;
+  return <div>Event Place Detail Page for {eventPlaceId}</div>;
 }
