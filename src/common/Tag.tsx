aaa5e327991c75c0973d3c6915c382902b93a4c5
JUNHORANG <sjsk3358@naver.com>
2026-03-30 17:40:12 +0900

    feat : 홈 메인 배너 이미지 작업

--- src/common/Tag.tsx
+++ src/common/Tag.tsx
@@ -9,7 +9,7 @@ export default function Tag({ children, className }: Props) {
     <>
       <div
         className={cn(
-          'text-text-default text-body4 md:text-body2 inline-block rounded-full bg-[#313131] px-2.25 py-0.5 md:px-3',
+          'text-body4 md:text-body2 text-text-default inline-block rounded-full bg-[#313131] px-2.25 py-0.5 md:px-3',
           className,
         )}
       >
